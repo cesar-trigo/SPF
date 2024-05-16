691a62e301feeebe3ab23a5bd36b46ab81fdf196
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-05-16 17:10:29 -0300

    creacion de modelo de user

---
+++ src/dao/models/userModelo.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  age: { type: Number, required: true },
+  role: { type: String, enum: ["admin", "user"], default: "user" },
+});
+
+export const userModelo = mongoose.model("users", userSchema);
