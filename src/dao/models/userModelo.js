bd6a72cd9f938767a7af71802188eee49212729a
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-06-07 00:30:25 -0300

    Modifies modifies user model
    The card property was added and the role property was modified

--- src/dao/models/userModelo.js
+++ src/dao/models/userModelo.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   first_name: { type: String, required: true },
   last_name: { type: String },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   age: { type: Number, required: true },
+  password: { type: String, required: true },
+  cart: { type: mongoose.Schema.Types.ObjectId, ref: "carts" },
   role: { type: String, enum: ["admin", "user"], default: "user" },
   githubId: { type: Boolean, default: false },
 });
