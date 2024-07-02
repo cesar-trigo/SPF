18e0621ca05bab322700e0f54cc6fa97f88c3b4a
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-07-02 17:53:48 -0300

    data fix

--- src/utils.js
+++ src/utils.js
@@ -8,10 +8,9 @@ const __dirname = dirname(__filename);
 
 export default __dirname;
 
-/* export const SECRET = "CoderCoder123";
 export const generaHash = password =>
   crypto.createHmac("sha256", SECRET).update(password).digest("hex");
- */
+
 export const createHash = async password => {
   const salt = await bcryptjs.genSalt(10);
   return bcryptjs.hash(password, salt);
