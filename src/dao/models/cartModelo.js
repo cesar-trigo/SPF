70e0e20d11d78c772b587ecf7be21246d85c595b
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-07-02 17:47:02 -0300

    model was refactored

--- src/dao/models/cartModelo.js
+++ src/dao/models/cartModelo.js
@@ -3,17 +3,18 @@ import mongoosePaginate from "mongoose-paginate-v2";
 
 const cartCollection = "cart";
 const cartSchema = new mongoose.Schema({
-    products: [{
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "products"
-        },
-        quantity: Number
-    }]
-},
-)
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "products",
+      },
+      quantity: Number,
+      _id: false,
+    },
+  ],
+});
 
 cartSchema.plugin(mongoosePaginate);
 
-
-export const cartModelo = mongoose.model(cartCollection, cartSchema)
+export const cartModelo = mongoose.model(cartCollection, cartSchema);
