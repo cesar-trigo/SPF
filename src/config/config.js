634c8b8e58dac0fe045750bf4d819c306f2aa7e5
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-07-02 17:43:52 -0300

    comments

--- src/config/config.js
+++ src/config/config.js
@@ -3,24 +3,28 @@ import { Command, Option } from "commander";
 import path from "path";
 import __dirname from "../utils.js";
 
-let program = new Command();
+// Configuración de Commander para opciones de línea de comandos
+const program = new Command();
 
 program.addOption(
   new Option("-m, --mode <modo>", "Script execution mode").choices(["dev", "prod"]).default("dev")
 );
+
 program.parse();
 
 const argument = program.opts();
 const mode = argument.mode;
 
+// Configuración de la ruta del archivo .env según el modo
 const envPath =
-  mode === "prod" ? path.join(__dirname, "./.env.dev") : path.join(__dirname, "./.env.prod");
+  mode === "prod" ? path.join(__dirname, "./.env.prod") : path.join(__dirname, "./.env.dev");
 
 dotenv.config({
   path: envPath,
   override: true,
 });
 
+// Exportación de la configuración
 export const config = {
   PORT: process.env.PORT || 3000,
   MONGO_URL: process.env.MONGO_URL,
