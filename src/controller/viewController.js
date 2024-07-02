6153ef5ad4cbc0a6e9fc48b00677cf72ef7a57e5
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-07-02 17:45:39 -0300

    the controller was refactored

--- src/controller/viewController.js
+++ src/controller/viewController.js
@@ -1,42 +1,29 @@
-import { isValidObjectId } from "mongoose";
-import { productService } from "../services/productService.js";
-import { productsModelo } from "../dao/models/productsModelo.js";
+import { lookToken } from "../config/passport.js";
 import { cartService } from "../services/cartService.js";
-import { io } from "../app.js";
+import { productService } from "../services/productService.js";
+import { UsersDTO } from "../dto/UsersDTO.js";
 
-export class vistasController {
-  static getProducts = async (req, res) => {
+export class ViewController {
+  static home = async (req, res) => {
     let products;
+    const token = lookToken(req);
     try {
       products = await productService.getProducts();
-      products = products.map(product => ({
-        title: product.title,
-        category: product.category,
-        price: product.price,
-        code: product.code,
-        _id: product._id,
-      }));
-    } catch (error) {
+    } catch {
       res.setHeader("Content-Type", "application/json");
       return res.status(500).json({
         error: `Error inesperado en el servidor - Intente más tarde, o contacte a su administrador`,
       });
     }
+
     res.setHeader("Content-Type", "text/html");
-    res.status(200).render("home", { products, user: "user" });
+    res.status(200).render("home", { products, login: token });
   };
 
-  static realtimeproducts = async (req, res) => {
+  static realTime = async (req, res) => {
     let products;
     try {
       products = await productService.getProducts();
-      products = products.map(product => ({
-        title: product.title,
-        category: product.category,
-        price: product.price,
-        code: product.code,
-        _id: product._id,
-      }));
     } catch (error) {
       console.log(error);
       res.setHeader("Content-Type", "application/json");
@@ -45,22 +32,46 @@ export class vistasController {
       });
     }
     res.setHeader("Content-Type", "text/html");
-    res.status(200).render("realTime", { products });
+    res.status(200).render("realTime", { products, login: req.user });
   };
 
   static chat = (req, res) => {
-    res.status(200).render("chat");
+    res.status(200).render("chat", { login: req.user });
   };
 
-  static getProductsPaginate = async (req, res) => {
+  static getProductsPaginate = async (searchQuery, options) => {
+    return productService.getProductsPaginate(searchQuery, options);
+  };
+
+  static buildLinks = (req, sort, products) => {
+    const { prevPage, nextPage } = products;
+    const baseUrl = req.originalUrl.split("?")[0];
+    const sortParam = sort ? `&sort=${sort}` : "";
+
+    const prevLink = prevPage ? `${baseUrl}?page=${prevPage}${sortParam}` : null;
+    const nextLink = nextPage ? `${baseUrl}?page=${nextPage}${sortParam}` : null;
+
+    return {
+      prevPage: prevPage ? parseInt(prevPage) : null,
+      nextPage: nextPage ? parseInt(nextPage) : null,
+      prevLink,
+      nextLink,
+    };
+  };
+
+  static getProducts = async (req, res) => {
+    const cartUser = req.user.cart;
     try {
-      const cart = (await productService.getProductsBy()) || (await productService.createCart());
+      /*       let cart = await cartService.getCartById(cartUser);
+      if (!cart) {
+        cart = await cartService.createCart();
+      } */
 
       const { page = 1, limit = 5, sort } = req.query;
 
       const options = {
-        page: Number(page) || 1,
-        limit: Number(limit) || 5,
+        page: Number(page),
+        limit: Number(limit),
         lean: true,
       };
 
@@ -85,21 +96,14 @@ export class vistasController {
         options.sort = { price: sort === "asc" ? 1 : -1 };
       }
 
-      let products = await productService.getProductsPaginate(searchQuery, options);
-      products.docs = products.docs.map(product => ({
-        title: product.title,
-        category: product.category,
-        price: product.price,
-        code: product.code,
-        _id: product._id,
-      }));
-
-      const { prevPage, nextPage, prevLink, nextLink } = vistasController.buildLinks(
+      const products = await ViewController.getProductsPaginate(searchQuery, options);
+      const { prevPage, nextPage, prevLink, nextLink } = ViewController.buildLinks(
         req,
         sort,
+        products,
         products
       );
-      const categories = await productsModelo.distinct("category");
+      const categories = await productService.getCategories();
 
       let requestedPage = parseInt(page);
       if (isNaN(requestedPage)) {
@@ -126,7 +130,8 @@ export class vistasController {
         prevLink,
         nextLink,
         categories,
-        cart,
+        cartUser,
+        login: req.user,
       });
     } catch (error) {
       console.error("Error:", error);
@@ -134,29 +139,13 @@ export class vistasController {
     }
   };
 
-  static buildLinks = (req, sort, products) => {
-    const { prevPage, nextPage } = products;
-    const baseUrl = req.originalUrl.split("?")[0];
-    const sortParam = sort ? `&sort=${sort}` : "";
-
-    const prevLink = prevPage ? `${baseUrl}?page=${prevPage}${sortParam}` : null;
-    const nextLink = nextPage ? `${baseUrl}?page=${nextPage}${sortParam}` : null;
-
-    return {
-      prevPage: prevPage ? parseInt(prevPage) : null,
-      nextPage: nextPage ? parseInt(nextPage) : null,
-      prevLink,
-      nextLink,
-    };
-  };
-
-  static getCartById = async (req, res) => {
-    const cid = req.user.cart;
-
+  static getCart = async (req, res) => {
+    const cid = req.params.cid;
     const cart = await cartService.getCartById({ _id: cid });
 
     if (cart) {
-      res.status(200).render("cart", { cart });
+      const plainCart = cart.toObject();
+      res.status(200).render("cart", { plainCart, login: req.user });
     } else {
       res.status(404).json({ error: `Cart not found with id: ${cid}` });
     }
@@ -174,33 +163,13 @@ export class vistasController {
     res.status(200).render("login", { error });
   };
 
-  static logout = (req, res) => {
-    res.clearCookie("codercookie", { httpOnly: true });
-    res.redirect("/login");
-  };
-
   static current = (req, res) => {
     const { user } = req;
+    const userDto = new UsersDTO(user);
     res.setHeader("Content-Type", "text/html");
     res.status(200).render("profile", {
-      user: user,
+      user: userDto,
+      login: req.user,
     });
-  };
-
-  static github = async (req, res) => {};
-
-  static callbackGithub = async (req, res) => {
-    if (!req.user) {
-      return res.redirect("/login");
-    }
-
-    req.session.user = {
-      first_name: req.user.first_name,
-      last_name: req.user.last_name,
-      age: req.user.age,
-      email: req.user.email,
-      role: req.user.role,
-    };
-    return res.redirect("/products");
   };
 }
