948118e61439e539c0663f2abae073bff706dca1
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-07-02 17:44:41 -0300

    the controller was refactored

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -1,9 +1,11 @@
-import { isValidObjectId } from "mongoose";
 import { cartService } from "../services/cartService.js";
 import { productService } from "../services/productService.js";
+import { isValidObjectId } from "mongoose";
+import { io } from "../app.js";
+import { ticketService } from "../services/ticketService.js";
 
-export class cartController {
-  static getCarts = async (req, res) => {
+export class CartController {
+  static getCart = async (req, res) => {
     try {
       res.setHeader("Content-Type", "application/json");
       const cart = await cartService.getCarts();
@@ -14,7 +16,7 @@ export class cartController {
     }
   };
 
-  static getCartById = async (req, res) => {
+  static getOneCart = async (req, res) => {
     try {
       res.setHeader("Content-Type", "application/json");
       const cid = req.params.cid;
@@ -56,20 +58,23 @@ export class cartController {
       });
     }
 
-    let productExists = await productService.getProductsById({ _id: pid });
+    const productExists = await productService.getProductsBy({ _id: pid });
     if (!productExists) {
       res.setHeader("Content-Type", "application/json");
       return res.status(400).json({ error: `No product exists with the ID: ${pid}` });
     }
 
-    let cartExists = await cartService.getCartById({ _id: cid });
+    const cartExists = await cartService.getCartById({ _id: cid });
     if (!cartExists) {
       res.setHeader("Content-Type", "application/json");
       return res.status(404).json({ error: `No cart exists with the ID: ${cid}` });
     }
+
     try {
-      let result = await cartService.addProductToCart(cid, pid);
-      res.status(200).json({ success: true, message: "Product added successfully", result });
+      let result = await cartService.addProductToCart(cartExists, pid);
+      res
+        .status(200)
+        .json({ success: true, message: "Product added successfully", result: result });
     } catch (error) {
       res.status(500).json({ error: `Unexpected server error`, detail: `${error.message}` });
     }
@@ -77,8 +82,8 @@ export class cartController {
 
   static updateCart = async (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    let cid = req.params.cid;
-    let products = req.body;
+    const cid = req.params.cid;
+    const products = req.body;
     if (!isValidObjectId(cid)) {
       return res.status(400).json({
         error: `Enter a valid MongoDB ID`,
@@ -99,7 +104,7 @@ export class cartController {
     }
   };
 
-  static updateProductQ = async (req, res) => {
+  static updateCartProductQuantity = async (req, res) => {
     res.setHeader("Content-Type", "application/json");
     const { cid, pid } = req.params;
     let { quantity } = req.body;
@@ -110,7 +115,7 @@ export class cartController {
       });
     }
 
-    let productExists = await productService.getProductsById({ _id: pid });
+    let productExists = await productService.getProductsBy({ _id: pid });
     if (!productExists) {
       res.setHeader("Content-Type", "application/json");
       return res.status(400).json({ error: `No product exists with the ID: ${pid}` });
@@ -123,14 +128,14 @@ export class cartController {
     }
 
     try {
-      const result = await cartService.updateProductQ(cid, pid, quantity);
+      const result = await cartService.updateCartProductQ(cid, pid, quantity);
       return res.status(200).json(result);
     } catch (error) {
       res.status(500).json({ error: `Unexpected server error`, detail: `${error.message}` });
     }
   };
 
-  static deleteAllProductsFromCart = async (req, res) => {
+  static deleteCart = async (req, res) => {
     res.setHeader("Content-Type", "application/json");
     const cid = req.params.cid;
 
@@ -147,7 +152,7 @@ export class cartController {
     }
 
     try {
-      let cartDeleted = await cartManager.deleteAllProductsFromCart(cid);
+      let cartDeleted = await cartService.deleteAllProductsFromCart(cid);
       if (cartDeleted) {
         res.status(200).json({ message: "All products removed from cart", cartDeleted });
       } else {
@@ -162,7 +167,7 @@ export class cartController {
     }
   };
 
-  static deleteProductFromCart = async (req, res) => {
+  static deleteCartProduct = async (req, res) => {
     res.setHeader("Content-Type", "application/json");
     const { cid, pid } = req.params;
 
@@ -172,10 +177,12 @@ export class cartController {
       });
     }
 
-    let productExists = await productService.getProductsById({ _id: pid });
+    let productExists = await productService.getProductsBy({ _id: pid });
     if (!productExists) {
       res.setHeader("Content-Type", "application/json");
-      return res.status(400).json({ error: `No product exists with the ID: ${pid}` });
+      return res
+        .status(400)
+        .json({ error: `No product exists with the ID: ${pid}, ${productExists}` });
     }
 
     let cartExists = await cartService.getCartById({ _id: cid });
@@ -194,6 +201,86 @@ export class cartController {
       }
     } catch (error) {
       res.status(500).json({ message: "Error deleting product from cart", error });
+    }
+  };
+
+  static purchase = async (req, res) => {
+    const { cid } = req.params;
+
+    if (!isValidObjectId(cid)) {
+      return res.status(400).json({ error: "ID de carrito no válido" });
+    }
+
+    try {
+      const cart = await cartService.getCartById({ _id: cid });
+
+      if (!cart) {
+        return res.status(404).json({ error: `Carrito con ID ${cid} no encontrado` });
+      }
+
+      const productsInCart = cart.products;
+      let productosParaFacturar = [];
+      let productosRestantes = [];
+
+      for (let product of productsInCart) {
+        const {
+          product: { _id: pid },
+          quantity,
+        } = product;
+
+        if (!isValidObjectId(pid)) {
+          return res.status(400).json({ error: `ID de producto no válido: ${pid}` });
+        }
+
+        const productData = await productService.getProductsBy({ _id: pid });
+
+        if (!productData) {
+          return res.status(404).json({ error: `Producto con ID ${pid} no encontrado` });
+        }
+
+        if (productData.stock < quantity) {
+          productosRestantes.push(product);
+        } else {
+          const newStock = productData.stock - quantity;
+          await productService.updateProduct(pid, { stock: newStock });
+
+          productosParaFacturar.push({
+            product: productData,
+            quantity,
+          });
+        }
+      }
+
+      const totalAmount = productosParaFacturar.reduce(
+        (total, item) => total + item.product.price * item.quantity,
+        0
+      );
+      const ticket = await ticketService.createTicket({
+        code: `T-${Date.now()}-${Math.random().toString(36).substring(2, 9)}`,
+        purchase_datetime: new Date(),
+        purchaser: req.user.email,
+        products: productosParaFacturar.map(item => ({
+          pid: item.product._id,
+          title: item.product.title,
+          price: item.product.price,
+          quantity: item.quantity,
+          subtotal: item.product.price * item.quantity,
+        })),
+        amount: totalAmount,
+      });
+
+      await cartService.updateCart(cid, productosRestantes);
+
+      return res.status(200).json({
+        message: "Compra realizada exitosamente",
+        products: productosParaFacturar,
+        productsRestantes: productosRestantes,
+        ticket,
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Error inesperado en el servidor", detalle: error.message });
     }
   };
 }
